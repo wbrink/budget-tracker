099c599de1af79f18278c76984d37d97c52b1f3d
wbrink <will.brink55@gmail.com>
2020-05-07 09:47:39 -0700

    updated packages and setup variables for heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+var MONGODB_URI = process.env.MONOGDB_URI || "mongodb://localhost/budget"
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -15,7 +17,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
