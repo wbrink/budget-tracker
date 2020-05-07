386643aba7f94ca87eaf60ac15281a7b1ee91331
wbrink <will.brink55@gmail.com>
2020-05-07 09:56:23 -0700

    fixed env variable typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-var MONGODB_URI = process.env.MONOGDB_URI || "mongodb://localhost/budget"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
 const PORT = process.env.PORT || 3000;
 
